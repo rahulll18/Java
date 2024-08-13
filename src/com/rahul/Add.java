c5624bc5f6157354418a63c751bdec19226d91d5
rahulll18 <109649389+rahulll18@users.noreply.github.com>
2024-08-13 23:29:32 +0530

    Changes made

--- src/com/rahul/Add.java
+++ src/com/rahul/Add.java
@@ -15,4 +15,5 @@ public class Add {
 
         System.out.println("Sum is :- " + (num1 + num2));
     }
+
 }
--- src/com/rahul/Armstrong.java
+++ src/com/rahul/Armstrong.java
@@ -1,2 +1,38 @@
-package com.rahul;public class Armstrong {
+package com.rahul;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+        public static void main(String[] args) {
+            //Scanner in = new Scanner(System.in);
+
+           // System.out.print("Enter the number :- ");
+            //int num = in.nextInt();
+
+            for(int i = 100 ; i < 1000 ; i++){
+                if(isArmStrong(i)){
+                    System.out.println(i);
+                }
+            }
+
+//            boolean check = isArmStrong(num);
+//            if(check){
+//                System.out.println(num + " is armstrong");
+//            }else{
+//                System.out.println(num + " is not armstrong");
+//            }
+        }
+
+        static boolean isArmStrong(int num){
+            int orginalN = num;
+            int ans = 0;
+            while(num > 0){
+                int rem =  num%10;
+                num /= 10;
+                ans = ans + rem * rem * rem;
+            }
+            return orginalN == ans;
+        }
+
 }
